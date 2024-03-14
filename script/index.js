fe24fcd311d2d1a8f49f1df81aa81ef8a835796c
Dowes Mbella <dowesw@gmail.com>
2024-03-13 20:28:14 -0400

    gestion section connecté

--- script/index.js
+++ script/index.js
@@ -1,16 +1,36 @@
 (function() {
+
+})();
+
+function onConnect() {
     let body = localStorage.getItem("user")
     if (!body) {
         body = sessionStorage.getItem("user")
     }
+    const profile = document.querySelector('#profile')
+    const btnlogin = document.querySelector('#btn-login')
+    if (!!body) {
+        btnlogin.classList.remove("visible")
+        btnlogin.classList.add("invisible")
+
+        profile.classList.remove("invisible")
+        profile.classList.add("visible")
+    } else {
+        btnlogin.classList.remove("invisible")
+        btnlogin.classList.add("visible")
+
+        profile.classList.remove("visible")
+        profile.classList.add("invisible")
+    }
     console.log(JSON.parse(body))
-})();
+}
 
 function imports(parent) {
     fetch(parent + 'header.html')
         .then(response => response.text())
         .then(content => {
             document.getElementById('header').innerHTML = content;
+            onConnect()
         })
         .catch(error => console.error('Erreur de chargement de la page externe:', error));
 
