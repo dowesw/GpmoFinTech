a18f5657100b2811ba85ec13bb4688738dd9ce10
akakanu <akakanu-leukeu-mboombhi.akakanu-leukeu-mboombhi.1@ulaval.ca>
2024-04-13 21:51:57 -0400

    modification des fichiers profil.js, .css, .html

--- script/profil.js
+++ script/profil.js
@@ -1,3 +1,35 @@
 (function() {
     imports('./../components/')
 })();
+
+function ouvreInfo(event, contentNameID) { 
+
+	let content = 
+		document.getElementsByClassName("contenuTab"); 
+	let totalCount = content.length; 
+
+	// Loop through the content class 
+	// and hide the tabs first 
+	for (let count = 0; 
+		count < totalCount; count++) { 
+		content[count].style.display = "none"; 
+	} 
+
+	let links = 
+		document.getElementsByClassName("tablinks"); 
+	totalLinks = links.length; 
+
+	// Loop through the links and 
+	// remove the active class 
+	for (let count = 0; 
+		count < totalLinks; count++) { 
+		links[count].classList.remove("active"); 
+	} 
+
+	// Display the current tab 
+	document.getElementById(contentNameID) 
+		.style.display = "block"; 
+
+	// Add the active class to the current tab 
+	event.currentTarget.classList.add("active"); 
+}
